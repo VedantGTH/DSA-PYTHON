84f6fd0692ab572af55219c1681ef662f7feccc1
Vedant Mali <vedantmali2006@gmail.com>
2025-12-30 10:40:02 +0530

    Add files via upload

---
+++ Arrays/TwoSum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        arr = [(nums[i], i) for i in range(len(nums))]   # store value + index
+        arr.sort()                                       # sort by value
+
+        left = 0
+        right = len(arr)-1
+
+        while left < right:
+            s = arr[left][0] + arr[right][0]
+
+            if s == target:
+                return [arr[left][1], arr[right][1]] 
+
+            elif s < target:
+                left += 1
+            else:
+                right -= 1
+
+        return []
+        
